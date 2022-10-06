665c672110b28023eeb44381f033fe883dfca517
mi-maezato <higaminato@gmail.com>
2022-10-06 21:32:57 +0900

    Q5〜Q7まで回答

--- task.rb
+++ task.rb
@@ -43,6 +43,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
+  p array1.empty?
+  p array2.empty?
 
 end
 
@@ -50,13 +52,15 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map { |numbers| numbers *10 }
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array.map!(&:to_i)
 
   # 以下は変更しないで下さい
   p array
