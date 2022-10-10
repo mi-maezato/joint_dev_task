a2d33d6f70638e7d31f746ddb7780a6436a1c091
mi-maezato <higaminato@gmail.com>
2022-10-10 20:23:49 +0900

    Q8~Q11まで回答

--- task.rb
+++ task.rb
@@ -70,7 +70,9 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
+  
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -80,6 +82,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 
 end
 
@@ -87,6 +92,14 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
+  foods.each do |food|
+    if food.include?("うに")
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
+
+  end
 
 end
 
@@ -94,6 +107,12 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  puts "ユーザーの趣味一覧"
+  sports.flatten!
+  sports.uniq!
+  sports.each.with_index(1) do |sport,i|
+    puts "No#{i} #{sport}"
+  end
 
 end
 
@@ -156,6 +175,7 @@ end
 
 class UserQ18
   # 以下に回答を記載
+
 
 end
 
